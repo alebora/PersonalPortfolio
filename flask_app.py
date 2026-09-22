89ace34c6896ea2f1cce40ba278e7e1b318ab322
alebora <148511093+alebora@users.noreply.github.com>
2026-09-22 13:55:15 -0400

    new version done

--- flask_app.py
+++ flask_app.py
@@ -1,26 +1,37 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, Blueprint, render_template
 
 app = Flask(__name__)
+
+legacy = Blueprint("legacy", __name__, url_prefix="/legacy")
+
+
+@legacy.route("/")
+def home():
+    return render_template("legacy/index.html")
+
+
+@legacy.route("/projects")
+def projects():
+    return render_template("legacy/projects.html")
+
+
+@legacy.route("/experience")
+def experience():
+    return render_template("legacy/experience.html")
+
+
+@legacy.route("/bloom")
+def bloom():
+    return render_template("legacy/bloom.html")
+
+
+app.register_blueprint(legacy)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-
-@app.route("/experience")
-def experience():
-    return render_template("experience.html")
-
-# @app.route('/j')
-# def valentine():
-#     return render_template('valentine.html')
-
-@app.route("/bloom")
-def bloom():
-    return render_template("bloom.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
