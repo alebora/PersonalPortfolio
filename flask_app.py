242eca11640811353181dd31a2732b08fa8ee2a1
alebora <148511093+alebora@users.noreply.github.com>
2026-09-22 10:23:27 -0400

    v2025-legacy

--- flask_app.py
+++ flask_app.py
@@ -14,10 +14,13 @@ def projects():
 def experience():
     return render_template("experience.html")
 
-@app.route('/j')
-def valentine():
-    return render_template('valentine.html')
+# @app.route('/j')
+# def valentine():
+#     return render_template('valentine.html')
 
+@app.route("/bloom")
+def bloom():
+    return render_template("bloom.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
